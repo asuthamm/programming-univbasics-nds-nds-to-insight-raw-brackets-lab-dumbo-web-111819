91d80f0367c801bd6db08d1901dc94f2cab8e689
Alex Suthammanont <asuthamm@gmail.com>
2019-10-27 01:27:05 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,13 +6,7 @@ def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
    
-    directors_database.each.with_index do |ele1,idx1|
-      directors_database[1][:movies].each.with_index do |ele2,idx2|
-        p directors_database[idx1][:movies][idx2][:worldwide_gross]
-      end
-      p "-"
-    end  
-    
+ 
     
     
     
