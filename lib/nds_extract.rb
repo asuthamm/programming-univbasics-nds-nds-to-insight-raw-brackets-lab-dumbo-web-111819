c9975c66d9043420ba81389d072ff33a2056ccb7
Alex Suthammanont <asuthamm@gmail.com>
2019-10-27 01:12:11 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,10 +1,22 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require 'pp'
 
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
-  #
+   
+    directors_database.each.with_index do |ele1,idx1|
+      directors_database[1][:movies].each.with_index do |ele2,idx2|
+        p directors_database[idx1][:movies][idx2][:worldwide_gross]
+      end
+      p "-"
+    end  
+    
+    
+    
+    
+    
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
@@ -18,5 +30,4 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
 end
